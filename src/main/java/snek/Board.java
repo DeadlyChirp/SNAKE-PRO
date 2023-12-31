5313efc0cdbc8fa5dc2ea5326dcc1b2782dc8f60
LongThanhTng <thanh-long.tang@etu.u-paris.fr>
2023-12-31 19:42:34 +0100

    snek simple done

--- src/main/java/snek/Board.java
+++ src/main/java/snek/Board.java
@@ -18,12 +18,14 @@ import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
-    private final int B_WIDTH = 500;
-    private final int B_HEIGHT = 500;
-    private final int DOT_SIZE = 10;
-    private final int ALL_DOTS = 900;
-    private final int RAND_POS = 29;
-    private final int DELAY = 140;
+    private final int DOT_SIZE = 20; // Increase the dot size for larger grid cells
+    private final int B_WIDTH = DOT_SIZE * 20; // Example: 20 grid cells wide
+    private final int B_HEIGHT = DOT_SIZE * 20; // Example: 20 grid cells tall
+    private final int ALL_DOTS = (B_WIDTH * B_HEIGHT) / (DOT_SIZE * DOT_SIZE); // Recalculate the maximum number of dots
+    private final int RAND_POS = (B_WIDTH / DOT_SIZE); // Recalculate the random position range
+
+
+    private final int DELAY = 200;
 
     private final int x[] = new int[ALL_DOTS];
     private final int y[] = new int[ALL_DOTS];
@@ -41,7 +43,10 @@ public class Board extends JPanel implements ActionListener {
     private Timer timer;
     private Image ball;
     private Image apple;
-    private Image head;
+    private Image headUp;
+    private Image headDown;
+    private Image headLeft;
+    private Image headRight;
 
 
     private int score = 0;
@@ -63,24 +68,32 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private void loadImages() {
-
-        ImageIcon iid = new ImageIcon("src/main/java/resources/dot.png");
+        ImageIcon iid = new ImageIcon(new ImageIcon("src/main/java/resources/snekbody.png").getImage().getScaledInstance(DOT_SIZE, DOT_SIZE, Image.SCALE_SMOOTH));
         ball = iid.getImage();
 
-        ImageIcon iia = new ImageIcon("src/main/java/resources/apple.png");
+        ImageIcon iia = new ImageIcon(new ImageIcon("src/main/java/resources/food.png").getImage().getScaledInstance(DOT_SIZE, DOT_SIZE, Image.SCALE_SMOOTH));
         apple = iia.getImage();
 
-        ImageIcon iih = new ImageIcon("src/main/java/resources/headsnek.png");
-        head = iih.getImage();
+        ImageIcon iihU = new ImageIcon("src/main/java/resources/head_up.png");
+        headUp = iihU.getImage().getScaledInstance(DOT_SIZE, DOT_SIZE, Image.SCALE_SMOOTH);
+
+        ImageIcon iihD = new ImageIcon("src/main/java/resources/head_down.png");
+        headDown = iihD.getImage().getScaledInstance(DOT_SIZE, DOT_SIZE, Image.SCALE_SMOOTH);
+
+        ImageIcon iihL = new ImageIcon("src/main/java/resources/head_left.png");
+        headLeft = iihL.getImage().getScaledInstance(DOT_SIZE, DOT_SIZE, Image.SCALE_SMOOTH);
+
+        ImageIcon iihR = new ImageIcon("src/main/java/resources/head_right.png");
+        headRight = iihR.getImage().getScaledInstance(DOT_SIZE, DOT_SIZE, Image.SCALE_SMOOTH);
     }
 
-    private void initGame() {
 
+    private void initGame() {
         dots = 3;
 
         for (int z = 0; z < dots; z++) {
-            x[z] = 50 - z * 10;
-            y[z] = 50;
+            x[z] = 0 - z * DOT_SIZE; // Start at 0 and go left for each subsequent dot
+            y[z] = DOT_SIZE; // Start one DOT_SIZE down from the top
         }
 
         locateApple();
@@ -90,6 +103,7 @@ public class Board extends JPanel implements ActionListener {
         updateScore();
     }
 
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -98,14 +112,49 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private void doDrawing(Graphics g) {
+        // Define colors for the checkerboard pattern
+        Color lightColor = new Color(208, 232, 161);  // A lighter green
+        Color darkColor = new Color(129, 140, 112);   // A darker green
+
+        // Draw the checkerboard pattern
+        for (int row = 0; row < B_HEIGHT / DOT_SIZE; row++) {
+            for (int col = 0; col < B_WIDTH / DOT_SIZE; col++) {
+                // Alternate between lightColor and darkColor
+                if ((row + col) % 2 == 0) {
+                    g.setColor(lightColor);
+                } else {
+                    g.setColor(darkColor);
+                }
+                g.fillRect(col * DOT_SIZE, row * DOT_SIZE, DOT_SIZE, DOT_SIZE);
+            }
+        }
+
+        // Draw the grid lines
+        g.setColor(Color.black);
+        for (int i = 0; i <= B_WIDTH; i += DOT_SIZE) {
+            g.drawLine(i, 0, i, B_HEIGHT); // Vertical lines
+        }
+        for (int i = 0; i <= B_HEIGHT; i += DOT_SIZE) {
+            g.drawLine(0, i, B_WIDTH, i); // Horizontal lines
+        }
 
         if (inGame) {
-
+            // Draw the apple ensuring it aligns with the grid
             g.drawImage(apple, apple_x, apple_y, this);
 
+            // Draw the snake ensuring each segment aligns with the grid
             for (int z = 0; z < dots; z++) {
                 if (z == 0) {
-                    g.drawImage(head, x[z], y[z], this);
+                    // Draw the head with the correct rotation
+                    if (leftDirection) {
+                        g.drawImage(headLeft, x[z], y[z], this);
+                    } else if (rightDirection) {
+                        g.drawImage(headRight, x[z], y[z], this);
+                    } else if (upDirection) {
+                        g.drawImage(headUp, x[z], y[z], this);
+                    } else if (downDirection) {
+                        g.drawImage(headDown, x[z], y[z], this);
+                    }
                 } else {
                     g.drawImage(ball, x[z], y[z], this);
                 }
@@ -114,7 +163,6 @@ public class Board extends JPanel implements ActionListener {
             Toolkit.getDefaultToolkit().sync();
 
         } else {
-
             gameOver(g);
         }
         if (inGame) {
@@ -124,15 +172,18 @@ public class Board extends JPanel implements ActionListener {
         }
     }
 
+
+
+
     private void drawScore(Graphics g) {
         String msg = "Score: " + score;
-        Font font = new Font("Serif", Font.ITALIC, 12);
+        Font font = new Font("Digital-7bo", Font.ITALIC, 12);
         FontMetrics metrics = getFontMetrics(font);
 
         int x = (B_WIDTH - metrics.stringWidth(msg)) - DOT_SIZE;
         int y = DOT_SIZE;
 
-        g.setColor(Color.white);
+        g.setColor(Color.red);
         g.setFont(font);
         g.drawString(msg, x, y);
     }
@@ -140,11 +191,11 @@ public class Board extends JPanel implements ActionListener {
 
     private void gameOver(Graphics g) {
 
-        String msg = "Game Over";
-        Font small = new Font("Helvetica", Font.BOLD, 14);
+        String msg = "Game Over."+ "\n" +"Buy starter pack to win!";
+        Font small = new Font("Digital-7", Font.BOLD, 18);
         FontMetrics metr = getFontMetrics(small);
 
-        g.setColor(Color.white);
+        g.setColor(Color.red);
         g.setFont(small);
         g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2);
         drawScore(g);
@@ -218,13 +269,13 @@ public class Board extends JPanel implements ActionListener {
     }
 
     private void locateApple() {
-
         int r = (int) (Math.random() * RAND_POS);
-        apple_x = ((r * DOT_SIZE));
+        apple_x = r * DOT_SIZE; // Ensure it's a multiple of DOT_SIZE
 
         r = (int) (Math.random() * RAND_POS);
-        apple_y = ((r * DOT_SIZE));
+        apple_y = r * DOT_SIZE; // Ensure it's a multiple of DOT_SIZE
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
